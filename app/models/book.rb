1a4f645ae3c84fbc1f017b5114309408c5001b8d
majikina131012 <majikina.ryuma.infratop@gmail.com>
2024-02-25 04:24:00 +0000

    移動

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,12 +4,12 @@ class Book < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :body, presence: true,
     length: { maximum: 200 }
-  
-  
+
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -17,18 +17,18 @@ class Book < ApplicationRecord
     end
     image
   end
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def favorites_count_for_last_week
     # 過去一週間のいいね合計カウントを計算する
     last_week_start = 1.week.ago
     last_week_end = Time.current
     self.favorites.where(created_at: last_week_start..last_week_end).count
   end
-  
+
   # 本日の投稿数をカウント
   scope :posted_today, -> {
     where("created_at >= ?", Time.zone.now.beginning_of_day)

f1ebe201619ac37e1af3cd055939f3ea4f7d992d
majikina131012 <majikina.ryuma.infratop@gmail.com>
2024-02-29 05:59:09 +0000

    コメントの非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # redirect_to book_path(book)
   end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.destroy
+    # BookComment.find(params[:id]).destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
